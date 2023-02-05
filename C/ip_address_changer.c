4396b1a4bc085c31574705052dc76572fb7b8c97
saintbassanaga <saintbassanaga01@gmail.com>
2023-02-05 10:25:23 +0100

    File runner function for Ip address swapping

---
+++ C/ip_address_changer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - IP address changer wrote in c
+ * @argc : argument number
+ * @argv : argument value
+ * Return: The swapped Ip Address.
+ */
+
+int main(int argc, char **argv)
+{
+	printf("IP ADDRESS CHANGER");
+	return (EXIT_SUCCESS);
+}
